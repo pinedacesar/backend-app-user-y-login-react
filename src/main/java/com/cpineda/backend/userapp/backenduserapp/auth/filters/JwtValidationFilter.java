27477259dcf1ee8a4e5f135d773774561d0da678
pinedacesar <cesar.pineda.assert@gmail.com>
2023-10-01 10:53:19 -0300

    se agrega el acceso a las rutas en el CorsConfigurationSource

--- src/main/java/com/cpineda/backend/userapp/backenduserapp/auth/filters/JwtValidationFilter.java
+++ src/main/java/com/cpineda/backend/userapp/backenduserapp/auth/filters/JwtValidationFilter.java
@@ -63,7 +63,7 @@ public class JwtValidationFilter extends BasicAuthenticationFilter {
             body.put("message", "El token no es valido!");
 
             response.getWriter().write(new ObjectMapper().writeValueAsString(body));
-            response.setStatus(403);
+            response.setStatus(401);
             response.setContentType("application/json");
         }
 
